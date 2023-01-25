99a348db58d7299a47081f1a401b5fca549422b2
Samantha <samanthadallagnol45@gmail.com>
2023-01-24 22:04:53 -0300

    projeto 2

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
 let nome;
+let idsetInterval;
+
+
+function erroaoenviarmensagem(erro){
+    console.log('ocorreu um erro');
+    console.log(erro);
+}
+
+function sucessoaoenviarmensagem(resposta){
+    console.log('deu bom');
+    console.log(resposta);
+} 
+
+
+
+function enviarmensagem(){
+    const texto = document.querySelector('.texto-input').value;
+    
+    const novaMensagem = {
+            from: nome,
+            to: "Texto",
+            text: texto,
+    }
+
+    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', novaMensagem)
+    promise.catch(erroaoenviarmensagem)
+    promise.then(sucessoaoenviarmensagem)
+}
+
 
 
 
 function renderizarMensagem(resposta){
     console.log(resposta)
-
-    
+   
 
 //pegar ul
 const container = document.querySelector('.mensagens');
@@ -103,10 +131,20 @@ function atualizarStatus(){
 
 function carregarChat(){
     nome = prompt("Qual o seu nome ?");
+
+    while(nome === ''){
+        alert('Nome invalido');
+        nome = prompt("Qual o seu nome ?");
+    }
+
     const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', {name:nome})
   
     promise.then(registrouCerto);
     promise.catch(erroAoRegistrar);
+
+
+
+
 
 }
 carregarChat();
